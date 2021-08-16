1117cb999758331d5114d8adb7f49967fd3b10a4
abduluptu <abduluptu@gmail.com>
2021-08-16 19:40:02 +0530

    This is My personal App. This is first update.

---
+++ app/src/main/java/com/sohainfotech/findallstay/data/utils/AbdulBhai.java
@@ -0,0 +1,7 @@
+package com.sohainfotech.findallstay.data.utils;
+
+public class AbdulBhai {
+    /**
+     * To update new code and remove Test class
+     */
+}
--- app/src/main/java/com/sohainfotech/findallstay/data/utils/Test.java
+++
@@ -1,7 +0,0 @@
-package com.sohainfotech.findallstay.data.utils;
-
-public class Test {
-    /**
-     * This class is created to check update code on Github
-     */
-}
